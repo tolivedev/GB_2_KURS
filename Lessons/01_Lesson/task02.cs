fa25817b4dab4da709ad78c3ec9279fbf5203050
tolivedev <tolive@live.ru>
2021-08-30 18:40:37 +0300

    01_les_init

---
+++ Lessons/01_Lesson/task02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons._01_Lesson
+{
+    class task02
+    {
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0; // O(1)
+            for (int i = 0; i < inputArray.Length; i++) // O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++) //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) //O(N)
+                    {
+                        int y = 0; // O(1)
+
+                        if (j != 0) 
+                        {
+                            y = k / j; // O(1)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; // O(1)
+                    }
+                }
+            }
+
+            return sum; //O(1)
+            // summary - 5+ N^3 = 5+ N^3 = N^3
+        }
+    }
+}
---
+++ Lessons/01_Lesson/task03.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons._01_Lesson
+{
+    public class TestCase
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Expected { get; set; }
+        public Exception ExpectedException { get; set; }
+    }
+
+
+
+    class task03
+    {
+        static int Fibon(int x, int y)
+        {
+            if (x < 0 || y < 0)
+            {
+                throw new ArgumentException("x and y must be positive");
+            }
+
+            return x + y;
+        }
+
+        static void TestSum(TestCase testCase)
+        {
+            try
+            {
+                var actual = Fibon(testCase.X, testCase.Y);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+
+        //static void Main(string[] args)
+        public void Start()
+        {
+            var testCase1 = new TestCase()
+            {
+                X = 6,
+                Y = 4,
+                Expected = 10,
+                ExpectedException = null
+            };
+
+            var testCase2 = new TestCase()
+            {
+                X = 1,
+                Y = 4,
+                Expected = 5,
+                ExpectedException = null
+            };
+
+            TestSum(testCase1);
+            TestSum(testCase2);
+        }
+    }
+}
