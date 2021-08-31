d1c06029cf35b84300209050c0570e671899d24b
tolivedev <tolive@live.ru>
2021-08-31 03:07:28 +0300

    01les_complete

--- Lessons/01_Lesson/task02.cs
+++ Lessons/01_Lesson/task02.cs
@@ -8,8 +8,12 @@ namespace Lessons._01_Lesson
 {
     class task02
     {
+        public task02()
+        {
+            StrangeSum(1,2,3,4);
+        }
 
-        public static int StrangeSum(int[] inputArray)
+        public static int StrangeSum(params int[] inputArray)
         {
             int sum = 0; // O(1)
             for (int i = 0; i < inputArray.Length; i++) // O(N)
@@ -29,8 +33,10 @@ namespace Lessons._01_Lesson
                     }
                 }
             }
+            Console.WriteLine("\nsummary - 5 + N ^ 3 = 5 + N ^ 3 = N ^ 3");
 
             return sum; //O(1)
+
             // summary - 5+ N^3 = 5+ N^3 = N^3
         }
     }
--- Lessons/01_Lesson/task03.cs
+++ Lessons/01_Lesson/task03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,6 @@ namespace Lessons._01_Lesson
     public class TestCase
     {
         public int X { get; set; }
-        public int Y { get; set; }
         public int Expected { get; set; }
         public Exception ExpectedException { get; set; }
     }
@@ -18,22 +18,52 @@ namespace Lessons._01_Lesson
 
     class task03
     {
-        static int Fibon(int x, int y)
+        public task03()
         {
-            if (x < 0 || y < 0)
+            Start();
+        }
+        static Stopwatch timer = new Stopwatch();
+        static int FibonachiRec(int n)
+        {
+            if (n == 0 || n == 1)
             {
-                throw new ArgumentException("x and y must be positive");
+                return n;
             }
-
-            return x + y;
+            else if (n < 0) { throw new ArgumentException(); }
+            else
+            {
+                return FibonachiRec(n - 1) + FibonachiRec(n - 2);
+            }
         }
 
-        static void TestSum(TestCase testCase)
+        static int Fibon(int end)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < end; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            if (a < 0)
+            {
+                throw new Exception();
+            }
+            return a;
+        }
+        static void TestFib(TestCase testCase)
         {
             try
             {
-                var actual = Fibon(testCase.X, testCase.Y);
-
+                timer.Start();
+                var actual = Fibon(testCase.X);
+                timer.Stop();
+                Console.WriteLine($"\nФибоначчи цикл за: {timer.Elapsed.Milliseconds} мс");
+                timer.Reset();
                 if (actual == testCase.Expected)
                 {
                     Console.WriteLine("VALID TEST");
@@ -57,27 +87,55 @@ namespace Lessons._01_Lesson
             }
         }
 
-        //static void Main(string[] args)
+        static void TestFibRec(TestCase testCase)
+        {
+            try
+            {
+                timer.Start();
+                var actual = FibonachiRec(testCase.X);
+                timer.Stop();
+                Console.WriteLine($"\nФибоначчи рекурсия за: {timer.Elapsed.Milliseconds} мс");
+                timer.Reset();
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("VALID TEST");
+            }
+            catch (Exception)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+
         public void Start()
         {
             var testCase1 = new TestCase()
             {
-                X = 6,
-                Y = 4,
-                Expected = 10,
+                X = 45,
+                Expected = 1134903170,
                 ExpectedException = null
             };
 
-            var testCase2 = new TestCase()
-            {
-                X = 1,
-                Y = 4,
-                Expected = 5,
-                ExpectedException = null
-            };
-
-            TestSum(testCase1);
-            TestSum(testCase2);
+            TestFib(testCase1);
+            TestFibRec(testCase1);
+            Console.ReadKey();
         }
     }
 }
